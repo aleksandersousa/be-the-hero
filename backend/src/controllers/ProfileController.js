ea03169671c754ff3752797002664a4595bdd7ef
souder-kun <aleksander-ssousa@outlook.com>
2020-03-29 17:50:06 -0300

    handle errors and integration tests

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -5,8 +5,8 @@ module.exports = {
     const ong_id = req.headers.authorization
 
     const incidents = await connection('incidents')
-    .where('ong_id', ong_id)
-    .select('*')
+      .where('ong_id', ong_id)
+      .select('*')
 
     return res.json(incidents)
   },
